ac3a11a1d1cdbe6375cf23900d5a9962848eacb3
David Tejedor <d.tejedor@proun.es>
2021-10-05 15:10:42 +0200

    Initial Commit

---
+++ AssetMaker/AssetMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetMakerApp.swift
+//  AssetMaker
+//
+//  Created by David on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AssetMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
